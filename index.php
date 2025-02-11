b01f604a05bbc4a7dbd62e1abe64a1c3139211f5
annibar17 <3120680@STUD.SRH-CAMPUS-BERLIN.DE>
2025-02-11 16:58:22 +0100

    my code
    my code for today

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>hello</h1>
+
+    <?php
+
+if ($_GET['page'] == 1) {
+    echo "
+    <a class='active' href='index.php?page=1'>Home</a>
+    <a class='' href='index.php?page=2'>About me</a>
+    <a class='' href='index.php?page=3'>Shop</a>
+    ";
+    require_once 'page1.php';
+}
+
+if ($_GET['page'] == 2) {
+    echo "
+    <a class='' href='index.php?page=1'>Home</a>
+    <a class='active' href='index.php?page=2'>About me</a>
+    <a class='' href='index.php?page=3'>Shop</a>
+    ";
+    require_once 'page2.php';
+}
+
+if ($_GET['page'] == 3) {
+    echo "
+    <a class='' href='index.php?page=1'>Home</a>
+    <a class='' href='index.php?page=2'>About me</a>
+    <a class='active' href='index.php?page=3'>Shop</a>
+    ";
+    require_once 'page3.php';
+}
+</body>
+</html>
